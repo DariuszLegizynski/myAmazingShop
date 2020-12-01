0b738944b88a544a79fe5210c063689f58d00de0
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2020-12-01 12:38:08 +0100

    adjusted the images for the carousel in the home intro. Also optimized all other images to be at smaller size.

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,45 +1,48 @@
 import React from "react";
 
+//styles
+import "./Intro.css";
+
 //slick-slider
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
+// images
+import sliderImage1 from "../../resources/images/MainTheme/ady-teenagerinro-sQ0xXxQdfeY-unsplash.jpg";
+import sliderImage2 from "../../resources/images/MainTheme/norbert-levajsics-dUx0gwLbhzs-unsplash.jpg";
+import sliderImage3 from "../../resources/images/MainTheme/joseph-gonzalez-bvteWf284Sw-unsplash.jpg";
+
 const Intro = () => {
 	const settings = {
 		infinite: true,
+		pauseOnHover:false,
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: true,
-		speed: 500,
+		speed: 1250,
 		autoplaySpeed: 3500,
 		fade: true,
 		cssEase: "linear",
 	  };
 
 	return (
-		<section clasName="intro">
+		<section className="intro">
 			<div className="intro__slider-container">
 				<Slider {...settings}>
 					<div>
-						<h3>1</h3>
+						<img className="intro__slider-container__image" src={sliderImage1} alt="backgroundImage1"/>
 					</div>
 					<div>
-						<h3>2</h3>
+						<img className="intro__slider-container__image" src={sliderImage2} alt="backgroundImage2"/>
 					</div>
 					<div>
-						<h3>3</h3>
-					</div>
-					<div>
-						<h3>4</h3>
-					</div>
-					<div>
-						<h3>5</h3>
-					</div>
-					<div>
-						<h3>6</h3>
+						<img className="intro__slider-container__image" src={sliderImage3} alt="backgroundImage3"/>
 					</div>
 				</Slider>
+			</div>
+			<div className="intro__text">
+				Intro Text
 			</div>
 		</section>
 	);
