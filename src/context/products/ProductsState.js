d52b02657689f5e193327fda56326e1de929974d
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2020-12-04 21:44:28 +0100

    Context and useReducer now working as expected. Getting the productList from the produtState.js

--- src/context/products/ProductsState.js
+++ src/context/products/ProductsState.js
@@ -1,40 +1,47 @@
-import React from "react";
-import { useReducer } from "react";
-// import { db } from "../../firebase/firebase";
+import { useReducer, useEffect } from "react";
+import { db } from "../../firebase/firebase";
 import ProductsContext from "./productsContext";
 import productsReducer from "./productsReducer";
 import {
-    SHOW_PRODUCTS,
-    // SET_LOADING
+	SHOW_PRODUCTS,
+	// SET_LOADING
 } from "../types";
 
 const ProductState = (props) => {
-    const initialState = {
-        products: [],
-        loading: false
-    }
+	const initialState = {
+		products: [],
+		loading: false,
+	};
 
-    const [ state, dispatch] = useReducer(productsReducer, initialState);
+	const [state, dispatch] = useReducer(
+		productsReducer,
+		initialState
+	);
 
-    const showProduct = (newProduct) => {
-        console.log(newProduct);
-        dispatch ({
-            type: SHOW_PRODUCTS,
-            payload: newProduct
-        })
-    }
+	const showProduct = (data) => {
+		dispatch({
+			type: SHOW_PRODUCTS,
+			payload: data,
+		});
+	};
 
-    return (
-        <ProductsContext.Provider
-        value={{
-            products: state.products,
-            loading: state.loading,
-            showProduct,
-        }}
-        >
-            {props.children}
-        </ProductsContext.Provider>
-    )
-}
+	useEffect(() => {
+		db.collection("products").onSnapshot((snapshot) => {
+			showProduct(snapshot.docs.map((doc) => doc.data()));
+		});
+	}, []);
+
+	return (
+		<ProductsContext.Provider
+			value={{
+				products: state.products,
+				loading: state.loading,
+				showProduct,
+			}}
+		>
+			{props.children}
+		</ProductsContext.Provider>
+	);
+};
 
 export default ProductState;
