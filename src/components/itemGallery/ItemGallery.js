d8bfac64fc7caca0a605f88bff26ad98aec6dbea
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2021-01-21 13:21:17 +0100

    finished refactoring hte item gallery (ItemGallery.js) in which the img are now clickable. Also remade its responsivness.

--- src/components/itemGallery/ItemGallery.js
+++ src/components/itemGallery/ItemGallery.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ItemGallery.css";
 
@@ -10,126 +10,44 @@ const ItemGallery = ({
 	imageTwoSmall,
 	imageThreeSmall,
 }) => {
-	const classHoverOne = () => {
-		document
-			.querySelector(
-				".item__wrapper__container__gallery__small__one"
-			)
-			.classList.toggle(
-				"item__wrapper__container__gallery__small__one--hidden"
-			);
-		document
-			.querySelector(
-				".item__wrapper__container__gallery__big__one"
-			)
-			.classList.toggle(
-				"item__wrapper__container__gallery__big__one--hidden"
-			);
-	};
+	const imagesBig = [imageOneBig, imageTwoBig, imageThreeBig];
+	const [selectedImg, setSelectedImg] = useState(imagesBig[0]);
 
-	const classHoverTwo = () => {
-		document
-			.querySelector(
-				".item__wrapper__container__gallery__small__two"
-			)
-			.classList.toggle(
-				"item__wrapper__container__gallery__small__two--selected"
-			);
-		document
-			.querySelector(
-				".item__wrapper__container__gallery__big__two"
-			)
-			.classList.toggle(
-				"item__wrapper__container__gallery__big__two--selected"
-			);
-	};
-
-	const classHoverThree = () => {
-		document
-			.querySelector(
-				".item__wrapper__container__gallery__small__three"
-			)
-			.classList.toggle(
-				"item__wrapper__container__gallery__small__three--selected"
-			);
-		document
-			.querySelector(
-				".item__wrapper__container__gallery__big__three"
-			)
-			.classList.toggle(
-				"item__wrapper__container__gallery__big__three--selected"
-			);
+	const toggleImg = (e) => {
+		setSelectedImg(imagesBig[e.target.alt]);
 	};
 
 	return (
-		<div className="item__wrapper__container__gallery">
-			<div className="item__wrapper__container__gallery__small">
-				<div
-					className="item__wrapper__container__gallery__small__one"
-					tabIndex="0"
-				>
-					<img src={imageOneSmall} alt="small-one" />
-				</div>
-				<div
-					className="item__wrapper__container__gallery__small__two"
-					onMouseOver={() => {
-						classHoverTwo();
-						classHoverOne();
-					}}
-					onMouseOut={() => {
-						classHoverTwo();
-						classHoverOne();
-					}}
-					tabIndex="0"
-					onFocus={() => {
-						classHoverTwo();
-						classHoverOne();
-					}}
-					onBlur={() => {
-						classHoverTwo();
-						classHoverOne();
-					}}
-				>
-					<img src={imageTwoSmall} alt="small-two" />
-				</div>
-				<div
-					className="item__wrapper__container__gallery__small__three"
-					onMouseOver={() => {
-						classHoverThree();
-						classHoverOne();
-					}}
-					onMouseOut={() => {
-						classHoverThree();
-						classHoverOne();
-					}}
-					tabIndex="0"
-					onFocus={() => {
-						classHoverThree();
-						classHoverOne();
-					}}
-					onBlur={() => {
-						classHoverThree();
-						classHoverOne();
-					}}
-				>
+		<section className="item__wrapper__container__center">
+			<div className="item__wrapper__container__center__gallery">
+				<div className="item__wrapper__container__center__gallery__small">
+					<img
+						src={imageOneSmall}
+						alt="0"
+						onClick={toggleImg}
+					/>
+
+					<img
+						src={imageTwoSmall}
+						alt="1"
+						onClick={toggleImg}
+					/>
+
 					<img
 						src={imageThreeSmall}
-						alt="small-three"
+						alt="2"
+						onClick={toggleImg}
+					/>
+				</div>
+				<div className="item__wrapper__container__center__gallery__big">
+					<img
+						className="item__wrapper__container__center__gallery__big__img"
+						src={selectedImg}
+						alt="big-one"
 					/>
 				</div>
 			</div>
-			<div className="item__wrapper__container__gallery__big">
-				<div className="item__wrapper__container__gallery__big__one">
-					<img src={imageOneBig} alt="big-one" />
-				</div>
-				<div className="item__wrapper__container__gallery__big__two">
-					<img src={imageTwoBig} alt="big-two" />
-				</div>
-				<div className="item__wrapper__container__gallery__big__three">
-					<img src={imageThreeBig} alt="big-three" />
-				</div>
-			</div>
-		</div>
+		</section>
 	);
 };
 
